5351da346f9a8a5388ba5b21a19ffee91ef1b470
sideking <jeremy-roy-cote@hotmail.com>
2024-10-17 00:59:32 -0400

    bit of tinkering

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ const swiper = new Swiper('.news__swiper', {
     },
   
   });
+
+let filterArr = document.querySelectorAll(".filter");
+console.log(filterArr)
+
+let projetsArr = document.querySelectorAll(".projects__element");
+console.log(projetsArr)
+
+for (let i = 0; i < filterArr.length; i++) {
+  filterArr[i].addEventListener("click", function() {
+    console.log(`${filterArr[i].id}`);
+    for (let j = 0; j < projetsArr.length; j++) {
+      projetsArr[j].style.display = "block";
+      if (!projetsArr[j].classList.contains(`${filterArr[i].id}`) ) {
+      projetsArr[j].style.display = "none";
+    }
+    }
+  })
+}

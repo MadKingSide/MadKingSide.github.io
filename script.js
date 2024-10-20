b9f8f35b8c1789252d082e78bc06da8c97492000
sideking <jeremy-roy-cote@hotmail.com>
2024-10-19 21:26:21 -0400

    working filter
    should still work with next additions

--- script.js
+++ script.js
@@ -25,14 +25,45 @@ console.log(filterArr)
 let projetsArr = document.querySelectorAll(".projects__element");
 console.log(projetsArr)
 
+
+
 for (let i = 0; i < filterArr.length; i++) {
   filterArr[i].addEventListener("click", function() {
-    console.log(`${filterArr[i].id}`);
+    //console.log(`${filterArr[i].id}`); // to check which class is clicked
+    filterArr[i].classList.toggle("toggled");
     for (let j = 0; j < projetsArr.length; j++) {
+      
       projetsArr[j].style.display = "block";
-      if (!projetsArr[j].classList.contains(`${filterArr[i].id}`) ) {
-      projetsArr[j].style.display = "none";
+
+      if (filterArr[i].id == "all") {
+        filterArr.forEach(element => {
+          element.classList.remove("toggled");
+        });
+        projetsArr.forEach(element => {
+          element.classList.remove("unDisplay");
+        });
+        filterArr[0].classList.add("toggled");
+      } else if (!projetsArr[j].classList.contains(`${filterArr[i].id}`) ) {
+        projetsArr[j].classList.toggle("unDisplay");
+        filterArr[0].classList.remove("toggled");
+      }
     }
+    //loop to check if a filter is selected (for tous)
+    let nbChecked = 0;
+    
+    for (let h = 0; h < filterArr.length; h++) {
+      
+      if (filterArr[h].classList.contains("toggled")) {
+        console.log("has");
+        nbChecked ++;
+      } else if (!filterArr[h].classList.contains("toggled")) {
+        console.log("none");
+      }
+      console.log(nbChecked) //number of filter checked
     }
+    if (nbChecked == 0) {
+      filterArr[0].classList.add("toggled");
+    }
+    
   })
 }
